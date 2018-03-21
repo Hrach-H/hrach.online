f20c11cfa9d4c7cf5a9cfd193db1022c10b133f6
hrachya.hovsepyan <hrach.hovsepyan@gmail.com>
2018-03-21 16:40:11 +0400

    Installed error tracking (Raven.js)

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Raven from 'raven-js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const ravenConfig = {
+    logger: 'my-app',
+    environment: 'development'
+};
+Raven.config('https://902e0f56d1634c52bd62ac82b246e9c7@sentry.io/361262', ravenConfig).install();
+Raven.context(() => {
+    ReactDOM.render(<App />, document.getElementById('root'));
+});
+
